b1b634e4a3af710b8144be5c4a20d217713d3249
EduardoMoreaes <17eduardo05@gmail.com>
2023-12-12 01:19:44 -0300

    commited with makefile

--- src/controller/db.rs
+++ src/controller/db.rs
@@ -11,7 +11,7 @@ pub fn get_db_config() -> Result<Config, Box<dyn Error>> {
 
     config.user = Some(var("DB_USER").unwrap_or_else(|_| "postgres".into()));
     config.password = Some(var("DB_PASSWORD").unwrap_or_else(|_| "postgres".into()));
-    config.dbname = Some(var("DB_NAME").unwrap_or_else(|_| "postgres".into()));
+    config.dbname = Some(var("DB_NAME").unwrap_or_else(|_| "Esmeralda".into()));
     config.host = Some(var("DB_HOSTNAME").unwrap_or_else(|_| "localhost".into()));
     config.manager = Some(ManagerConfig {
         recycling_method: RecyclingMethod::Fast,
@@ -47,7 +47,6 @@ impl DataBase {
                 })?,
             };
             
-        println!("Database conectado!");
         Ok(db)
     }
 
