34de8e2e0de8fd04a58a1bf83b6ab6808a837807
EduardoMoreaes <17eduardo05@gmail.com>
2023-12-16 21:26:55 -0300

    commited with makefile

--- src/model/structs.rs
+++ src/model/structs.rs
@@ -9,3 +9,12 @@ pub struct User{
     pub username: String,
     pub password: String
 }
+
+pub struct Info{
+    pub debtor: String,
+    pub value: f64,
+    pub date_in: String,
+    pub date_out: String,
+    pub installments: String,
+    pub status: bool
+}
--- src/view/routes/home.rs
+++ src/view/routes/home.rs
@@ -1,8 +1,89 @@
+use dioxus_elements::br;
+
+use crate::structs::Info;
 use super::*;
 
 #[component]
 pub fn Home (cx: Scope) -> Element {
+    let total_counts = use_state(cx, || 0.0);
+    let counts: Vec<Info> = Vec::new();
+
     render!{
-        h1{ "Logado" }
+        link{ r#rel: "stylesheet", href: "./src/view/styles/home.css" }
+
+        h2{ id: "name",  "Esmeralda" }
+
+        
+
+        div{ id: "div-body",
+            div{ id: "div-table",
+                table{ id: "table-counts", 
+                    th{ format!("Contas: {}", total_counts) }
+                    tr{
+                        td{ "Devedor" },
+                        td{ "Valor" },
+                        td{ "Data Inicial" },
+                        td{ "Data Final" },
+                        td{ "Parcelas" },
+                        td{hidden: true, "Status" }
+                    }
+                    for info in counts {
+                        tr{
+                            td{hidden: true, format!("{}", info.debtor) },
+                            td{ format!("{:2}", info.value) },
+                            td{ format!("{}", info.date_in) },
+                            td{ format!("{}", info.date_out) },
+                            td{ format!("{}", info.installments) },
+                            td{ format!("{}", info.status) },
+                        }
+                    }
+                }
+            }
+
+            div{ id: "div-optiions", 
+                button{ "Adicionar" },
+                button{ "Pagar" },
+                button{ "Editar" },
+                button{ "Exportar" },
+
+                p{
+                    label{
+                        "Devedor:" br{}
+                        input{ r#type: "text", id: "debtor" }
+                    }
+
+                    br{}
+                    
+                    label{
+                        "Valor:" br{}
+                        input{ r#type: "numeric", id: "value" }
+                    }
+                }
+
+                p{
+                    label{
+                        "Data:" br{}
+                        input{ r#type: "date", id: "date_in" }
+                        " - até - "
+                        input{ r#type: "date", id: "date_out" }
+                    }
+
+                }
+
+                p{
+                    label{
+                        "Parcelas:"
+                        input{  r#type: "number", id: "installments" }
+                    }
+
+                    br{}
+
+                    label{
+                        "Já tá pago?"
+                        input{ r#type: "checkbox", id: "payment" }
+                    }
+                }
+            }
+        }
     }
 }
