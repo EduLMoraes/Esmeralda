62e818643bd4a7cbfdbf9df02a45b67a2293853a
EduLMoraes <17eduardo05@gmail.com>
2024-05-04 20:16:17 -0300

    commited with makefile

---
+++ install_to_linux.sh
@@ -0,0 +1,8 @@
+cd $HOME
+mkdir .esmeralda
+cd .esmeralda
+wget https://github.com/EduLMoraes/Esmeralda/releases/download/v1.0.0/esmeralda_1.0.0_amd64.deb
+dpkg -i esmeralda_1.0.0_amd64.deb
+wget https://github.com/EduLMoraes/Esmeralda/releases/download/v1.0.0/sources.zip
+unzip sources.zip
+rm sources.zip
