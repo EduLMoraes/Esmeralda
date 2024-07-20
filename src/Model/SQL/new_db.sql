6b1a8da60e066170156e2fed00ecee8597bc5ec8
EduLMoraes <17eduardo05@gmail.com>
2024-07-20 18:01:57 -0300

    Feedback of last month rentable

--- src/Model/SQL/new_db.sql
+++ src/Model/SQL/new_db.sql
@@ -7,7 +7,8 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(200) NOT NULL,
-    name VARCHAR(100) NOT NULL
+    name VARCHAR(100) NOT NULL,
+    last_login DATE
 );
 
 CREATE TABLE IF NOT EXISTS counts (
