6640ac63285c738aa50641459f243865567e4b49
EduLMoraes <17eduardo05@gmail.com>
2024-08-31 14:31:39 -0300

    Add version on screen

--- src/Views/templates/boxes/to_home/menu_left/ml_head.rs
+++ src/Views/templates/boxes/to_home/menu_left/ml_head.rs
@@ -4,6 +4,11 @@ use crate::env::var;
 pub fn ml_head() -> Box {
     let box_head = Box::new(Orientation::Vertical, 0);
     box_head.add_css_class("box_head_ml");
+    let version = Label::new(Some(&format!(
+        "Versão {}",
+        std::env::var("CARGO_PKG_VERSION").unwrap()
+    )));
+    version.add_css_class("version");
 
     let box_title = Box::new(Orientation::Horizontal, 0);
     box_title.add_css_class("box_title_ml");
@@ -16,6 +21,7 @@ pub fn ml_head() -> Box {
 
     box_title.append(&icon);
     box_title.append(&title);
+    box_title.append(&version);
 
     box_title.set_halign(gtk::Align::Center);
 
